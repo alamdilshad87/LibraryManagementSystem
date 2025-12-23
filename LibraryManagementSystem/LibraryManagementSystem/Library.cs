dbca2c3d0ffccbfb6aa0e540d752913175b69833
alamdilshad87 <dilshadand@gmail.com>
2025-12-23 14:53:26 +0530

    Class  Creation

---
+++ LibraryManagementSystem/LibraryManagementSystem/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public bool IsAvailable { get; set; }
+}
+public class Member
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Book> BorrowedBooks { get; set; } = new List<Book>();
+}
